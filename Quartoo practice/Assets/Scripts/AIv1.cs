4ecaead303901880c248766037b20d262e3f329e
levimason96 <lmason2@harding.edu>
2020-02-13 22:42:09 -0600

    Added Second Half of AIv1

--- Quartoo practice/Assets/Scripts/AIv1.cs
+++ Quartoo practice/Assets/Scripts/AIv1.cs
@@ -6,22 +6,32 @@ using UnityEngine.UI;
 
 public class AIv1 : MonoBehaviour
 {
-    //private GameController gameController;
+    private GameController gameController;
 
-    //Unity's getMove for AI
-    //public void getMove(char[] arr)
-    //{
-    //    List<GamePiece> availablePieces = gameController.availablePieces;
-    //    int numOfAvailablePieces = availablePieces.Count;
+    // Unity's Phase 1 for AI
+    public void setAIpiece()
+    {
+        List<GamePiece> availablePieces = gameController.availablePieces;
+        int numOfAvailablePieces = availablePieces.Count;
 
-    //    System.Random rand = new System.Random();
-    //    int option = rand.Next(numOfAvailablePieces);
-    //    GamePiece chosenMove = availablePieces[option];
+        System.Random rand = new System.Random();
+        int option = rand.Next(numOfAvailablePieces);
+        GamePiece chosenMove = availablePieces[option];
 
-    //    gameController.SetSelectedPiece(chosenMove);
-    //}
+        gameController.SetSelectedPiece(chosenMove);
+    }
+
+    // Unity's Phase 2 for AI
+    public void setAIposition()
+    {
+        Button[] availablePositions = gameController.buttonList;
+        int numOfAvailablePositions = availablePositions.Length;
 
 
-    //Button[] buttonList;
+        System.Random rand = new System.Random();
+        int option = rand.Next(numOfAvailablePositions);
+        Button chosenPosition = availablePositions[option];
 
+        gameController.SetRecentMove(chosenPosition);
+    }
 }
