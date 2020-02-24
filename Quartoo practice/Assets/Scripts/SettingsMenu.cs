82fded8b60fc41277fe2ace299a6d00e9dfb65fe
tdurey <tdurey1@harding.edu>
2020-02-24 14:40:43 -0600

    Added additional UI elements to settings tab

--- Quartoo practice/Assets/Scripts/SettingsMenu.cs
+++ Quartoo practice/Assets/Scripts/SettingsMenu.cs
@@ -5,10 +5,16 @@ using UnityEngine.Audio;
 public class SettingsMenu : MonoBehaviour
 {
     public Slider volume;
-    public AudioMixer audioMixer;
+    //public AudioMixer audioMixer;
 
-    public void SetVolume (float volume)
+    public void SetMusicVolume (float volume)
     {
-        audioMixer.SetFloat("volume", volume);
+        //audioMixer.SetFloat("volume", volume);
+        Debug.Log(volume);
+    }
+
+    public void SetSoundEffectsVolume (float volume)
+    {
+        Debug.Log(volume);
     }
 }
