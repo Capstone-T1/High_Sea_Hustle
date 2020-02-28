557d3783d44b395457111972a5c98125f611a004
tdurey <tdurey1@harding.edu>
2020-02-28 12:23:08 -0600

    Added functional volume sliders in settings

--- Quartoo practice/Assets/Scripts/SettingsMenu.cs
+++ Quartoo practice/Assets/Scripts/SettingsMenu.cs
@@ -1,21 +1,52 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
     public Slider musicSlider;
     public Slider soundEffectsSlider;
-    //public AudioMixer audioMixer;
+    public AudioMixer masterMixer;
 
-    public void SetMusicVolume (float volume)
+    void Awake()
     {
-        //audioMixer.SetFloat("volume", volume);
-        Debug.Log(volume);
+        float savedMusicVol;
+        float savedSoundFXVol;
+        
+        savedMusicVol = PlayerPrefs.GetFloat(("prefsMusicVolume"), musicSlider.maxValue / 2);
+        Debug.Log(PlayerPrefs.GetFloat(("prefsMusicVolume"), musicSlider.maxValue / 2));
+        savedSoundFXVol = PlayerPrefs.GetFloat(("prefsSoundFXVolume"), soundEffectsSlider.maxValue / 2);
+
+        // Manually set value & volume before subscribing to ensure it is set even if slider.value happens to start at the same value as is saved
+        SetMusicVolume(savedMusicVol);
+        SetSoundFXVolume(savedSoundFXVol);
+
+        musicSlider.value = savedMusicVol;
+        soundEffectsSlider.value = savedSoundFXVol;
     }
 
-    public void SetSoundEffectsVolume (float volume)
+    public void SetMusicVolume(float volume)
     {
-        Debug.Log(volume);
+        masterMixer.SetFloat("musicVolume", ConvertToDecibel(volume / musicSlider.maxValue)); //Dividing by max allows arbitrary positive slider maxValue
+        PlayerPrefs.SetFloat("prefsMusicVolume", volume);
+    }
+
+    public void SetSoundFXVolume(float volume)
+    {
+        masterMixer.SetFloat("soundFXVolume", ConvertToDecibel(volume / soundEffectsSlider.maxValue)); //Dividing by max allows arbitrary positive slider maxValue
+        PlayerPrefs.SetFloat("prefsSoundFXVolume", volume);
+    }
+
+    //  Converts a percentage fraction to decibels,
+    // with a lower clamp of 0.0001 for a minimum of -80dB, same as Unity's Mixers.
+    public float ConvertToDecibel(float volume)
+    {
+        return Mathf.Log10(Mathf.Max(volume, 0.0001f)) * 20f;
+    }
+
+    void OnApplicationQuit()
+    {
+        PlayerPrefs.DeleteKey("prefsMusicVolume");
+        PlayerPrefs.DeleteKey("prefsSoundFXVolume");
     }
 }
