bbab529769ebce5b3527b8fe533d68993e203aa8
levimason96 <31603963+levimason96@users.noreply.github.com>
2020-03-29 18:20:48 -0500

    Added checkForWinPosition()

--- Quartoo practice/Assets/Scripts/AIHard.cs
+++ Quartoo practice/Assets/Scripts/AIHard.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 public class AIHard
 {
+    private GameCore gameCore = new GameCore();
     /*
         *************************
         Phase 1 (Pick a place for piece given)
@@ -29,7 +30,7 @@ public class AIHard
                     the possibility for a win or prevent as many possible losses?
                     I play aggressive, leaving those areas open, but I am open to suggestions on this.
     */
-    public string choosePosition(List<GameCore.BoardSpace> availableBoardSpaces)
+    public string choosePosition(List<GameCore.BoardSpace> availableBoardSpaces, GameCore.Piece pieceGivenToAI)
     {
         string chosenLocation = null;
         int numOfAvailablePositions = availableBoardSpaces.Count;
@@ -38,12 +39,12 @@ public class AIHard
         if (numOfAvailablePositions <= 13)
         {
             //Check to see if peice given leads to win
-
-            //if(Condition met)
-            //{
-            //  chosenLocation = said location;
-            return chosenLocation;
-            //}
+            string winningLocation = checkForWinPosition(pieceGivenToAI, availableBoardSpaces);
+            if(winningLocation != null)
+            {
+                chosenLocation = winningLocation;
+                return chosenLocation;
+            }
         }
 
         //When AI goes second, choosing very first location
@@ -117,5 +118,120 @@ public class AIHard
 
         viablePieces.Clear();
         return chosenPiece;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public string checkForWinPosition(GameCore.Piece givenPiece, List<GameCore.BoardSpace> availableSpaces)
+    {
+        string winningPosition = null;
+        GameCore.Piece[][] AITempBoard = gameCore.GetGameBoard();
+        List<GameCore.BoardSpace> tempList = availableSpaces;
+
+        for (int i = 0; i < (tempList.Count-1); i++)
+        {
+            AITempBoard[tempList[i].row][tempList[i].col] = givenPiece;
+
+            // checks the rows
+            for (int j = 0; j < AITempBoard.Length; j++)
+            {
+                GameCore.Piece[] result = AITempBoard[j];
+                if (checkWinConditions(result[0], result[1], result[2], result[3]))
+                {
+                    winningPosition = tempList[i].id;
+                    return winningPosition;
+                }
+            }
+
+            // checks the cols
+            for (int j = 0; j < AITempBoard.Length; j++)
+            {
+                GameCore.Piece[] result = new GameCore.Piece[4];
+                for (int k = 0; k < 4; k++)
+                    result[j] = AITempBoard[k][j];
+                if (checkWinConditions(result[0], result[1], result[2], result[3]))
+                {
+                    winningPosition = tempList[i].id;
+                    return winningPosition;
+                }
+            }
+
+            // checks the main diagonal (left to right)
+            if (checkWinConditions(AITempBoard[0][0], AITempBoard[1][1], AITempBoard[2][2], AITempBoard[3][3]))
+            {
+                winningPosition = tempList[i].id;
+                return winningPosition;
+            }
+
+            //// checks the secondary diagonal (right to left)
+            if (checkWinConditions(AITempBoard[0][3], AITempBoard[1][2], AITempBoard[2][1], AITempBoard[3][0]))
+            {
+                winningPosition = tempList[i].id;
+                return winningPosition;
+            }
+
+            //Resetting Location
+            AITempBoard[tempList[i].row][tempList[i].col] = new GameCore.Piece(2, 0, 0, 0, "");
+        }
+
+        return winningPosition;
+    }
+
+    //*****************************************************************************************************
+    //Taken from GameCore.cs
+    private bool checkWinConditions(GameCore.Piece a, GameCore.Piece b, GameCore.Piece c, GameCore.Piece d)
+    {
+        // checks if the other gameBoard of the game board are empty (no GamePieces on them)          
+        if (a.color == 2 || b.color == 2 || c.color == 2 || d.color == 2)
+            return false;
+
+        // checks if there are 4 GamePieces next to each other with similiar stats
+        if (a.color == b.color && a.color == c.color && a.color == d.color)
+        {
+            Debug.Log("Possible win by color");
+            return true;
+        }
+
+        else if (a.height == b.height && a.height == c.height && a.height == d.height)
+        {
+            Debug.Log("Possible win by height");
+            return true;
+        }
+        else if (a.shape == b.shape && a.shape == c.shape && a.shape == d.shape)
+        {
+            Debug.Log("Possible win by shape");
+            return true;
+        }
+        else if (a.emblem == b.emblem && a.emblem == c.emblem && a.emblem == d.emblem)
+        {
+            Debug.Log("Possible win by emblem");
+            return true;
+        }
+        // if there arent any conditions met, that means that there isn't a winner
+        return false;
     }
 }
